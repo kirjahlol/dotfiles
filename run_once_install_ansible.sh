7eb45d93b40a9beb6f727287d579edd4e4e24bad
kirjahlol <150558369+kirjahlol@users.noreply.github.com>
2026-04-05 17:32:53 -0400

    feat: add automation scripts

---
+++ run_once_install_ansible.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+os="$(uname -s)"
+case "$os" in
+  Linux*)
+    if [ -f /etc/arch-release ]; then
+      sudo pacman -S ansible
+    else
+      echo "Unsupported Linux distribution"
+      exit 1
+    fi
+    ;;
+  *)
+    echo "Unsupported operating system"
+    exit 1
+    ;;
+esac
+
+echo "Ansible installation done"
+
+ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
